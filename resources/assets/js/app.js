0ce7a11079672203b09f2adc1e6194bf008f91a1
Neo Ighodaro <neo@creativitykills.co>
2017-07-08 11:44:01 +0100

    Codacy fix

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -5,9 +5,9 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+require("./bootstrap");
 
-window.Vue = require('vue');
+window.Vue = require("vue");
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -15,8 +15,8 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example', require('./components/Example.vue'));
+Vue.component("example", require("./components/Example.vue"));
 
 const app = new Vue({
-    el: '#app'
+    el: "#app"
 });
