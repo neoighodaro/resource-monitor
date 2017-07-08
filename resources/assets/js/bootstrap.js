eecfd805032dcf22c35e0cf7aa93335003ece56d
Neo Ighodaro <neo@creativitykills.co>
2017-07-08 12:49:52 +0100

    Codacy fix for bootstrap file

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -11,7 +11,9 @@ try {
     window.$ = window.jQuery = require('jquery');
 
     require('bootstrap-sass');
-} catch (e) {}
+} catch (e) {
+    // Shh!
+}
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
