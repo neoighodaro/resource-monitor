10a725e650c664ba6e8dae4a677f80a3abd4d23d
Neo Ighodaro <neo@creativitykills.co>
2017-07-09 13:00:37 +0100

    Removed csrf

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends BaseVerifier
      * @var array
      */
     protected $except = [
-        '/resources/status'
+        '/resources/records',
+        '/resources/status',
     ];
 }
