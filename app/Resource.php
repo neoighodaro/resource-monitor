ddda05f8c897d932a37b57bb9a00698600a3e27b
Neo Ighodaro <neo@creativitykills.co>
2017-10-09 11:09:57 +0100

    Clean up

--- app/Resource.php
+++ app/Resource.php
@@ -31,26 +31,24 @@ class Resource extends Model {
     {
         $resource_starts   = isset( $this->resource_starts) ?  $this->resource_starts : '00:00';
         $resource_ends     = isset( $this->resource_ends) ?  $this->resource_ends : '24:00';
-        if($this->exclude_weekends == 1){
+
+        if ($this->exclude_weekends == 1) {
             return $this->whereId($this->id)->with(['records' => function ($record) use ($startDate, $endDate, $resource_starts, $resource_ends ) {
                 $record->whereBetween('created_at', [$startDate->toDateString(), $endDate->addDay(1)->toDateString()])
                         ->whereRaw('TIME(created_at) >= ?', $resource_starts)
                         ->whereRaw('TIME(created_at) <= ?', $resource_ends)
                         ->whereRaw('WEEKDAY( DATE(created_at) ) < ?', 5)
-                        ->orderBy('created_at', 'desc')
-                        ->paginate(10);
+                        ->orderBy('created_at', 'desc');
                 }])->first();
-        }
-        else{
+        } else {
             return $this->whereId($this->id)->with(['records' => function ($record) use ($startDate, $endDate, $resource_starts, $resource_ends ) {
                 $record->whereBetween('created_at', [$startDate->toDateString(), $endDate->addDay(1)->toDateString()])
                         ->whereRaw('TIME(created_at) >= ?', $resource_starts)
                         ->whereRaw('TIME(created_at) <= ?', $resource_ends)
-                        ->orderBy('created_at', 'desc')
-                        ->paginate(10);
-                }])->first();          
+                        ->orderBy('created_at', 'desc');
+                }])->first();
         }
-              
+
     }
 
     /**
